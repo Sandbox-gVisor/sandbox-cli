324eb87c1b4b3cd0a7fc18b06e44dbfdd9263ca6
BigCubeCat <bigcubecat@yandex.ru>
2023-07-24 09:53:29 +0700

    fix(infos): print

--- cli/socket.go
+++ cli/socket.go
@@ -23,8 +23,5 @@ func SendToSocket(addr string, message []byte) ([]byte, error) {
 		fmt.Println("Ошибка при отправке JSON-запроса серверу:", err)
 		return nil, err
 	}
-	var buff []byte
-	buff, err = ioutil.ReadAll(conn)
-	fmt.Println(string(buff))
-	return buff, err
+	return ioutil.ReadAll(conn)
 }
