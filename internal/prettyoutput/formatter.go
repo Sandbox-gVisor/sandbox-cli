d704e7319a7e42a8aee5def6faf3d78f5d71cdb2
AleksandrMatsko <a.matsko@g.nsu.ru>
2024-02-27 12:15:11 +0700

    refactor: remove unused struct

--- internal/prettyoutput/formatter.go
+++ internal/prettyoutput/formatter.go
@@ -34,7 +34,7 @@ func (handler *DefaultResponseFormatter) Format(response *communication.Response
 		responseType = MakeTextBold(response.Type)
 	}
 
-	// map is not used because we won't special order of headers to be printed
+	// map is not used because we want special order of headers to be printed
 	type header struct {
 		name  string
 		value string
